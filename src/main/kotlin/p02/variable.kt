1cec6ba08eb4e43b59a7e8bfc39a78e6ad467d1d
Masahiro Atsumi <raindrop.aqua@gmail.com>
2018-05-30 22:31:55 +0900

    add variable

--- src/main/kotlin/p02/variable.kt
+++ src/main/kotlin/p02/variable.kt
@@ -1,10 +1,13 @@
 package p02
 
 fun main(args: Array<String>) {
-    val input: String = null
+    val input: String? = null
     trim(input)
 }
 
-fun trim(src: String): String {
+fun trim(src: String?): String {
+    if (src == null) {
+        throw NullPointerException()
+    }
     return src.trim()
 }
