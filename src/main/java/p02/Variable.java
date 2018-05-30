dfc4e499ab79220258aa8d814d7f325d20225ba9
Masahiro Atsumi <raindrop.aqua@gmail.com>
2018-05-30 22:29:28 +0900

    add variable

---
+++ src/main/java/p02/Variable.java
@@ -0,0 +1,13 @@
+package p02;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        String input = null;
+        trim(input);
+    }
+
+    private static String trim(String src) {
+        return src.trim();
+    }
+}
---
+++ src/main/kotlin/p02/variable.kt
@@ -0,0 +1,10 @@
+package p02
+
+fun main(args: Array<String>) {
+    val input: String = null
+    trim(input)
+}
+
+fun trim(src: String): String {
+    return src.trim()
+}
