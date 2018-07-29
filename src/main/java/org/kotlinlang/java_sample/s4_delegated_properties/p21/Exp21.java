0ba1063c7fab4cbba2c87370cc4e1c455c97472b
Masahiro Atsumi <raindrop.aqua@gmail.com>
2018-07-29 17:39:40 +0900

    演習追加

--- src/main/java/org/kotlinlang/java_sample/s4_delegated_properties/p21/Exp21.java
+++ src/main/java/org/kotlinlang/java_sample/s4_delegated_properties/p21/Exp21.java
@@ -1,4 +1,16 @@
 package org.kotlinlang.java_sample.s4_delegated_properties.p21;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exp21 {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("John Doe", 25);
+
+        User user = new User(map);
+
+        String template = "name = %s, age = %d";
+        System.out.println(String.format(template, user.getName(), user.getAge()));
+    }
 }
--- src/main/java/org/kotlinlang/java_sample/s4_delegated_properties/p21/User.java
+++ src/main/java/org/kotlinlang/java_sample/s4_delegated_properties/p21/User.java
@@ -1,4 +1,24 @@
 package org.kotlinlang.java_sample.s4_delegated_properties.p21;
 
+import java.util.Map;
+
 public class User {
+    private String name;
+    private Integer age;
+
+    public User(Map<String, Integer> map) {
+        for (String key : map.keySet()) {
+            this.name = key;
+            this.age = map.get(key);
+            break;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
 }
